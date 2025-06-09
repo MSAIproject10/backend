6dd4942be7043365520144f85018380df77a1dde
sshooting0203 <sshl505@naver.com>
2025-06-09 12:05:07 +0900

    6_9_deploy

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,6 +3,10 @@ from fastapi import FastAPI
 from backend.app.routers import auth, parkings, search_logs, usages, vehicles, user
 
 app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "API test!"}
 app.include_router(auth.router, prefix="/auth")
 app.include_router(usages.router, prefix="/usages")
 app.include_router(parkings.router, prefix="/parkings")
