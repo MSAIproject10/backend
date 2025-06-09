e72e684e03063ad860fa387632fb6347bf2528e9
sshooting0203 <sshl505@naver.com>
2025-06-09 13:14:04 +0900

    6_9_13

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,15 @@
 # uvicorn main:app --reload
-from fastapi import FastAPI
-from backend.app.routers import auth, parkings, search_logs, usages, vehicles, user
+# from fastapi import FastAPI
+# from backend.app.routers import auth, parkings, search_logs, usages, vehicles, user
 
-app = FastAPI()
+# app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "API test!"}
-app.include_router(auth.router, prefix="/auth")
-app.include_router(usages.router, prefix="/usages")
-app.include_router(parkings.router, prefix="/parkings")
-app.include_router(vehicles.router, prefix="/vehicles")
-app.include_router(search_logs.router, prefix="/search-logs")
-app.include_router(user.router, prefix="/user")
+# @app.get("/")
+# def read_root():
+#     return {"message": "API test!"}
+# app.include_router(auth.router, prefix="/auth")
+# app.include_router(usages.router, prefix="/usages")
+# app.include_router(parkings.router, prefix="/parkings")
+# app.include_router(vehicles.router, prefix="/vehicles")
+# app.include_router(search_logs.router, prefix="/search-logs")
+# app.include_router(user.router, prefix="/user")
--- run.py
+++ run.py
@@ -1,1 +1,14 @@
-from backend.app.main import app
+from fastapi import FastAPI
+from backend.app.routers import auth, parkings, search_logs, usages, vehicles, user
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "API test!"}
+app.include_router(auth.router, prefix="/auth")
+app.include_router(usages.router, prefix="/usages")
+app.include_router(parkings.router, prefix="/parkings")
+app.include_router(vehicles.router, prefix="/vehicles")
+app.include_router(search_logs.router, prefix="/search-logs")
+app.include_router(user.router, prefix="/user")
