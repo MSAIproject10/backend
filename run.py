88aaf6d82afb65cc3a2b5224ffe5716edbcdfabf
sshooting0203 <sshl505@naver.com>
2025-06-12 14:11:58 +0900

    6_12

--- run.py
+++ run.py
@@ -7,7 +7,8 @@ app = FastAPI()
 origins = [
     "http://localhost:8080",     
     "http://127.0.0.1:8080",
-    "https://tenrian-web-app-fnb0djb2dygpbse0.koreasouth-01.azurewebsites.net",      # Flutter Web 개발 서버
+    "https://tenrian-web-app-fnb0djb2dygpbse0.koreasouth-01.azurewebsites.net",  
+    "https://lively-water-0a1c2c800.6.azurestaticapps.net",      # Flutter Web 개발 서버
 ]
 
 app.add_middleware(
